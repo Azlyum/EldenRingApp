6e3ae33e14dd93a20ed2430f6e456e289d67a1e4
Azlyum <81360724+Azlyum@users.noreply.github.com>
2024-06-02 01:22:35 +0100

    small refactor

--- src/App.js
+++ src/App.js
@@ -27,9 +27,11 @@ function App() {
     <div className="App-contents">
       <h1>Elden Ring Project</h1>
       <p>
-        This is a Project i created for myself <br />I am also wanting to show
-        off one of my favourite games in the process <br />I hope you will find
-        this page usefull and interesting as i delve deep into elden ring lore
+        This is a Project i created for myself 
+        <br />I am also wanting to show
+        off one of my favourite games in the process 
+        <br />I hope you will find
+        this page useful and interesting as i delve deep into elden ring lore
       </p>
     </div>
     <div>
